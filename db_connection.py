c4d0a1670ba381c8bba074f60e6fd63520b54cee
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-12 14:37:04 +0100

    Added DB Connection Class

---
+++ db_connection.py
@@ -0,0 +1,31 @@
+import pyodbc
+
+
+class DBConnection:
+    def __init__(self, database="master", server='databases2.spartaglobal.academy', username='SA', password='Passw0rd2018'):        # Establish connection with any db in MSsql
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+        # Making connection & Cursor
+        self.conn = self._establish_connection()
+        self.cursor = self.conn.cursor()
+
+    def set_new_database(self, database_name):
+        self.__create_database(database_name)
+        self.database = database_name
+
+    def __create_database(self, database_name):
+        self.sql_query(f"CREATE DATABASE {database_name};")
+        self.conn.commit()
+
+    def _establish_connection(self):
+        conn = 'DRIVER={SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password
+        return pyodbc.connect(conn)
+
+    # open to sql injections - use a separate method to validate string first
+    def __sql_query_execute(self, sql_string):
+        return self.cursor.execute(sql_string)
+
+    def sql_query(self, sql_string):
+        return self.__sql_query_execute(sql_string)
